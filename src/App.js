713c8e6e54603424aa8b15950002bd5fd332fa8b
Daniel Vidal <dvidalriezu@gmail.com>
2021-03-21 17:35:32 +0100

    adding tabs, styling and logic to switch display of components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,62 @@
 import React, { Component } from 'react';
 
 import { CardList } from './components/card-list/card-list.component';
+import Coins from './components/coins/coins.component';
 
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
-      customers: []
+      customers: [],
+      displayCustomers: true,
+      displayCoins: false,
     };
   }
 
+  // As I don't know anything about Django REST Framework I used this API to get fake customer information
+  // The pictures are taken from another API in the card component
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(users => this.setState({ customers: users }));
+    console.log(this.state.displayCustomers);
+    console.log(this.state.displayCoins);
+  }
+
+  //logic to change display in container App
+  changeToCustomers = () => {
+    if(this.state.displayCoins) {
+      this.setState({
+        displayCustomers: true,
+        displayCoins: false,
+      });
+    }
+  }
+
+  changeToCoins = () => {
+    if(this.state.displayCustomers) {
+      this.setState({
+        displayCustomers: false,
+        displayCoins: true,
+      });
+    }
   }
 
   render() {
     return(
       <div className='App'>
-        <CardList customers={this.state.customers} />
+        <div className="tab-container">
+          <button className="tab-button" onClick={this.changeToCustomers}> CUSTOMERS </button>
+          <button className="tab-button" onClick={this.changeToCoins}> COINS </button>
+        </div>
+        {this.state.displayCustomers ? (
+          <CardList customers={this.state.customers} />
+        ) : (
+          <Coins />
+        )}
       </div>
     )
   }
---
+++ src/components/coins/coins.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './coins.styles.css';
+
+class Coins extends React.Component {
+
+  render() {
+    return <h1 className="coins-container">COINS</h1>
+  }
+}
+
+export default Coins;
+
